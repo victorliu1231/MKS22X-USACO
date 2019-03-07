f27e2c2364cadd527642f663b010e9389235c4a1
Victor Liu <vliu00@stuy.edu>
2019-03-06 22:13:13 -0500

    realized that my way of parsing files is terrible...

--- USACO.java
+++ USACO.java
@@ -20,9 +20,11 @@ public class USACO{
         //dw about file instructions cuz he's only inputting valid files
         try{
           int index = 0;
-          int[][] board = new int[0][0]; //just to initialize
+          ArrayList<ArrayList<Integer>> board = new ArrayList<>(); //just to initialize
           String R = "";
-          int[] values = new int[4];
+          int C = 0;
+          ArrayList<Integer> values = new ArrayList<>();
+          ArrayList<ArrayList<Integer>> instructions = new ArrayList<>();
           File file = new File(filename);
           Scanner s = new Scanner(file);
           while (s.hasNextLine()){
@@ -31,34 +33,64 @@ public class USACO{
               String storer = "";
               int i = 0;
               while (i != firstLine.length()){
-                //values[i/2] = Integer.parseInt(firstLine.substring(i,i+1));
                 if (firstLine.charAt(i) == ' '){
+                  values.add(Integer.parseInt(storer));
                   storer = "";
+                } else {
+                  storer+= firstLine.charAt(i);
                 }
-                storer+= firstLine.charAt(i);
-                value.add(firstLine);
+                if (i == firstLine.length() - 1){
+                  values.add(Integer.parseInt(storer));
+                }
+                i++;
               }
-              R = values.get(0);
-              int C = values[1];
-              int E = values[2];
-              int N = values[3];
-              board = new int[R][C];
+              R = ""+values.get(0);
+              C = values.get(1);
+              int E = values.get(2);
+              int N = values.get(3);
             }
-            if (index-1 < R){
+            else if (index-1 < Integer.parseInt(R)){
               String boardLine = s.nextLine();
-              for (int i = 0; i < boardLine.length(); i+=2){
-                board[index-1][i/2] = Integer.parseInt(boardLine.substring(i,i+1));
+              board.add(new ArrayList<Integer>());
+              String storer = "";
+              int i = 0;
+              while (i != boardLine.length()){
+                if (boardLine.charAt(i) == ' '){
+                  board.get(index-1).add(Integer.parseInt(storer));
+                  storer = "";
+                } else {
+                  storer+= boardLine.charAt(i);
+                }
+                if (i == boardLine.length() - 1){
+                  board.get(index-1).add(Integer.parseInt(storer));
+                }
+                i++;
+              }
+            }
+            else {
+              String instructLine = s.nextLine();
+              instructions.add(new ArrayList<Integer>());
+              String storer = "";
+              int i = 0;
+              while (i != instructLine.length()){
+                if (instructLine.charAt(i) == ' '){
+                  instructions.get(index-Integer.parseInt(R)-1).add(Integer.parseInt(storer));
+                  storer = "";
+                } else {
+                  storer+= instructLine.charAt(i);
+                }
+                if (i == instructLine.length() - 1){
+                  instructions.get(index-Integer.parseInt(R)-1).add(Integer.parseInt(storer));
+                }
+                i++;
               }
             }
             index++;
           }
-
-          //testing
-          System.out.println(print(values));
-          System.out.println(print(board));
         } catch (FileNotFoundException e){
           System.out.println("Use a valid file!");
         }
+        //reading in the file is an absolute mess...
         return -1; //dummy value
     }
 
@@ -69,42 +101,40 @@ public class USACO{
         try{
           File file = new File(filename);
           Scanner s = new Scanner(file);
-          while (s.hasNextLine()){
-
+          int index = 0;
+          int N = 0; //to initialize
+          int M = 0;
+          int T = 0;
+          String[][] grid = new String[][]{};
+          while (s.hasNext()){
+            if (index == 0){
+              N = s.next();
+            }
+            if (index == 1){
+              M = s.next();
+            }
+            if (index == 2){
+              T = s.next();
+              grid = new String[N][M];
+            }
+            if (index > 2 && index <= 2 + N){
+              grid[index-2] = s.next(); //index-2 is to calibrate index in regards to grid index
+            }
+            if (index == 2 + N + 1){
+              
+            }
+            index++;
+            System.out.println(s.next());
           }
 
-
+          //System.out.println(values);
+          //System.out.println(board);
+          //System.out.println(instructions);
         } catch (FileNotFoundException e){
           System.out.println("Use a valid file!");
         }
+        //reading in the file is an absolute mess...
         return -1; //dummy value
     }
 
-    public static String print(int[] ary){
-      String ans = "[";
-      for (int i = 0; i < ary.length; i++){
-        ans+= ary[i];
-        if (i != ary.length - 1){
-          ans+= ",";
-        }
-      }
-      return ans;
-    }
-
-    public static String print(int[][] ary){
-      String ans = "[\n";
-      for (int r = 0; r < ary.length; r++){
-        ans+= " [";
-        for (int c = 0; c < ary[r].length; c++){
-          ans+= ary[r][c];
-          if (c != ary[r].length-1){
-            ans+=",";
-          }
-        }
-        if (r != ary.length - 1){
-          ans+= "]\n";
-        }
-      }
-      return ans;
-    }
 }
--- VDriver.java
+++ VDriver.java
@@ -1,5 +1,6 @@
 public class VDriver{
   public static void main(String[]args){
     USACO.bronze("makelake.in");
+    USACO.silver("ctravel.1.in");
   }
 }
